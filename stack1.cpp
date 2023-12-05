813a06e5c1347cc10f9ec974c9b99b8079cd81c6
noobsixt9 <86828330+noobsixt9@users.noreply.github.com>
2023-12-05 08:43:46 +0545

    Add files via upload

---
+++ stack1.cpp
@@ -0,0 +1,88 @@
+#include<stdio.h>
+
+#define SIZE 4
+int top = -1, stack[SIZE];
+
+void push()
+{
+	if(top == SIZE - 1 ) //checking if stack is full or not
+	
+	printf("Stack is full");
+	else
+	{
+		int n;
+		printf("Enter the data: ");
+		scanf("%d",&n);
+		top++;
+		stack[top]=n;
+		}	
+	
+}
+
+void pop()
+{
+	int popped;
+	if(top == -1)
+	printf("Stack is empty");
+	else
+	{
+		popped= stack[top];
+		top--;
+		printf("%d is popped from the stack",popped);
+	}
+}
+
+void peek()
+{
+	if(top==-1)
+	printf("Stack is empty nothing to peek!");
+	else
+	{
+		printf("top of the stack %d",stack[top]);
+	}
+}
+
+
+void displaystck()
+{
+	for(int i=top; i>=0; i--)
+	{
+		printf("\n\nprinting all data from stack stack[%d]: %d\n",i,stack[i]);
+	}
+}
+
+
+int main()
+{
+	
+	int ch;
+	
+	do
+	{
+	printf("Enter choice: ");
+	scanf("%d",&ch);
+		switch(ch)
+		{
+			case 1:
+				push();
+				break;
+			case 2:
+				pop();
+				break;
+			case 3:
+				peek();
+				break;
+			default:
+				printf("wrong choice");
+		}
+	}while(ch!=0);
+//	for(int i=0;i<=SIZE; i++)
+//	{
+//		push();
+//	}
+//displaystck();
+//printf("\n");
+//pop();
+//displaystck();
+	return 0;
+}
